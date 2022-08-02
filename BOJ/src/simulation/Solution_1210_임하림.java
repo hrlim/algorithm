8433b6a9cac79d634cca4428246876d6312de01b
임하림 <gkgid0840@naver.com>
2022-08-02 20:31:53 +0900

    [ERR방지] SWEA 문제

---
+++ BOJ/src/simulation/Solution_1210_임하림.java
@@ -0,0 +1,64 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * SWEA 1210 Ladder1
+ * 
+ * @author hrlim
+ * @version 1.0, 2022.08.02
+ */
+
+public class Solution_1210_임하림 {
+
+	public static int[][] dirs2 = { { 0, 1 }, { 0, -1 } };
+
+	public static int[][] map = new int[100][100];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			br.readLine();
+
+			int startX = -1;
+			for (int i = 0; i < 100; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int length = st.countTokens();
+				for (int j = 0; j < length; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+					if (map[i][j] == 2) {
+						startX = j;
+					}
+				}
+			}
+
+			for (int curY = 98; curY > 0; curY--) {
+				int movedX = getMovedX(startX, curY, 100);
+				if (movedX == startX) {
+					continue;
+				}
+				startX = movedX;
+			}
+			System.out.println("#" + test_case + " " + startX);
+		}
+
+	}
+
+	static int getMovedX(int x, int y, int N) {
+		int movedX = x;
+		for (int i = 0; i < dirs2.length; i++) {
+			int tempX = x + dirs2[i][1];
+			while (tempX >= 0 && tempX < N && map[y][tempX] != 0) {
+				movedX = tempX;
+				tempX += dirs2[i][1];
+			}
+		}
+		return movedX;
+	}
+
+}
---
+++ BOJ/src/simulation/Solution_1954_임하림.java
@@ -0,0 +1,63 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+/**
+ * 
+ * SWEA 1954 달팽이 숫자
+ * 
+ * @author hrlim
+ * @version 1.0, 2022.08.02
+ */
+
+public class Solution_1954_임하림 {
+
+	// 0 : 오른쪽, 1 : 아래, 2 : 왼쪽 , 3 : 위
+	public static final int[] dx = { 0, 1, 0, -1 };
+	public static final int[] dy = { 1, 0, -1, 0 };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int N = Integer.parseInt(br.readLine());
+
+			if (N == 1) {
+				System.out.println("#" + test_case);
+				System.out.println("1");
+				continue;
+			}
+
+			int[][] arr = new int[N][N];
+			int direction = 0;
+			int curX = 0, curY = 0;
+			
+			for (int i = 0; i < N * N; i++) {
+				arr[curX][curY] = i + 1;
+				curX += dx[direction];
+				curY += dy[direction];
+				if ((curX < 0 || curX >= N || curY < 0 || curY >= N) || arr[curX][curY] != 0) {
+					curX -= dx[direction];
+					curY -= dy[direction];
+
+					direction = (direction + 1) % 4;
+
+					curX += dx[direction];
+					curY += dy[direction];
+				}
+			}
+			
+			System.out.println("#" + test_case);
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					System.out.print(arr[i][j] + " ");
+				}
+				System.out.println();
+			}
+		}
+	}
+}
